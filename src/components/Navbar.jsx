9c2630d79a7bd392eb2b1bfd08d8f0f0ec8a2551
ROFABS <159296862+ROFABS@users.noreply.github.com>
2024-10-27 17:07:04 +0530

    Update Navbar.jsx
    jj

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   return (
     <>
       <nav>
-        <div className="logo">Zomato</div>
+        <div className="logo">ROFABS</div>
         <div className={show ? "navLinks showmenu" : "navLinks"}>
           <div className="links">
             {data[0].navbarLinks.map((element) => (
